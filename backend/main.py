eadcd28cb15eb9349e827317c7122993b9bf3c2e
smartpools <rherrera@smartpools.pe>
2025-06-14 14:54:36 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ import os
 
 app = FastAPI()
 HUGGINGFACE_API_KEY = os.getenv("HF_API_KEY")
-API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom-560m"
+API_URL = "https://api-inference.huggingface.co/models/mrm8488/t5-base-finetuned-common_gen"
 HEADERS = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
 
 class UserMessage(BaseModel):
